b6b2cb1be6301e48007c7715d274032dafd3f272
John42sp <32909234+John42sp@users.noreply.github.com>
2019-06-17 18:38:03 -0300

    palestras main index
    mudança

--- main.js
+++ main.js
@@ -17,7 +17,26 @@ $(document).ready(function(){
       });
     });
 
-  
+    $(document).ready(function(){
+      $(".p0fecha").click(function(){
+        $(".p0robson").hide();
+      });
+      $("#p0abre").click(function(){
+        $("#p0robson").show();
+      });
+    });
+
+    $(document).ready(function(){
+      $(".p3fecha").click(function(){
+        $(".p3andrea").hide();
+      });
+      $("#p3abre").click(function(){
+        $("#p3andrea").show();
+      });
+    });
+
+
+
 
 
 
