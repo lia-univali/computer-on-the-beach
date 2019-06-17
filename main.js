5e851acd403e6b53eee909269e9e553d84c00338
John42sp <32909234+John42sp@users.noreply.github.com>
2019-06-17 15:00:02 -0300

    atualização

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 
+/*$(document).ready(() => { 
+      $('#p1').on('click', () => {
+      $('#p1michel').show();
+  });     
+      $('#pfecha').on('click', () => {
+      $('#p1michel').hide();
+  }); 
+});*/
 
-      
- $(document).ready(() => {
-       $("#p1").on('click',() => {
-             $("#p1michel").toggle();
-        }); 
- });
+$(document).ready(function(){
+      $(".p1fecha").click(function(){
+        $(".p1michel").hide();
+      });
+      $("#p1abre").click(function(){
+        $("#p1michel").show();
+      });
+    });
 
-
-
-
-
+  
 
 
 
