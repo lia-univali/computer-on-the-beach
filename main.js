66caa2fdca0e89f2904229fedd8b4f89fa4aa84f
Alisson Steffens <alissonsteffens@MacBook-Pro-de-Alisson.local>
2019-09-07 02:28:32 -0300

    :art: new menu style

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
     $(window).on('scroll', onScroll)
 })
 function onScroll() {
-    if (navOffsetTop < $(window).scrollTop() && !$('nav').hasClass('fixed')) {
-        $('nav').addClass('fixed')
-        $('body').css("padding-top", $('nav').height())
+    if (navOffsetTop < $(window).scrollTop() && !$('nav').hasClass('is-fixed-top')) {
+        $('nav').addClass('is-fixed-top')
+        $('body').css("padding-top", $('nav').height()-50)
     }
-    if (navOffsetTop > $(window).scrollTop() && $('nav').hasClass('fixed')) {
-        $('nav').removeClass('fixed')
+    if (navOffsetTop > $(window).scrollTop() && $('nav').hasClass('is-fixed-top')) {
+        $('nav').removeClass('is-fixed-top')
         $('body').css("padding-top", 0)
     }
 }
