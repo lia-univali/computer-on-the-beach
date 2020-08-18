adc4cad54adaca80fbb97209bd04025d30be2391
AlissonSteffens <ali.steffens@gmail.com>
2020-08-18 01:37:52 -0300

    :tada: add talks

---
+++ panels.js
@@ -0,0 +1,28 @@
+function openContent(evt, tabName) {
+    // var i, x, tablinks;
+    // x = document.getElementsByClassName("content-tab");
+    // for (i = 0; i < x.length; i++) {
+    //     x[i].style.display = "none";
+    // }
+    // tablinks = document.getElementsByClassName("tab");
+    // for (i = 0; i < x.length; i++) {
+    //     tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+    // }
+    var doc = document.getElementById(tabName);
+    var content = null;
+    for (var i = 0; i < doc.childNodes.length; i++) {
+        if (doc.childNodes[i].className == "card-content") {
+            content = doc.childNodes[i];
+          break;
+        }        
+    }
+    var display = content.style.display;
+    if (doc.className.includes('is-active')){
+        // content.style.display = 'none';
+        doc.className = doc.className.replace(" is-active", "");
+    }else{
+        // content.style.display = 'block';
+        doc.className += " is-active";
+    }
+    // evt.currentTarget.className += " is-active";
+}
